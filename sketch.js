6df21e7ffda139ce892a0c9b9e266c5bfa683d19
vanka-t <vt2068@nyu.edu>
2021-04-12 09:46:22 -0400

    starting to add buttons

--- sketch.js
+++ sketch.js
@@ -4,16 +4,20 @@ var myVid;
 var objectResults = [];
 var pixelFont;
 var cnv;
+var buttonYes, buttonNo, buttonGuess;
+var bx, by, xOffset, yOffset, overButton, locked, buttonSize; //adjustments for mousePressed for buttonYes
 
 function preload(){
   //img = loadImage("images/cleanboi.jpg");
   pixelFont = loadFont("assets/font2.ttf");
   detector = ml5.objectDetector("cocossd");
+  buttonNo = loadImage("images/incorrect.png");
+  buttonYes = loadImage("images/correct.png");
 }
 
 function setup() {
   
-  cnv = createCanvas(1280, 720);
+  cnv = createCanvas(1280, 1000);
  // cnv.mouseClicked();
   
   //img.resize(width,height);
@@ -21,7 +25,10 @@ function setup() {
     video: {
       mandatory: {
         minWidth: width,
-        minHeight: height
+        //width: { min: 640, ideal: width},
+        minHeight: 720
+        //height: { min: 600, ideal: 720 },
+        //frameRate: { max: 30 }
       },
     },
   };
@@ -48,8 +55,23 @@ function draw() {
   textFont(pixelFont);
   textSize(50);
   textAlign(CENTER);
-
-  
+buttonSize = 100;
+  if ( //inspired by p5 examples
+    mouseX > bx - buttonSize &&
+    mouseX < bx + buttonSize &&
+    mouseY > by - buttonSize &&
+    mouseY < by + buttonSize
+  ) {
+    overButton = true;
+    if (!locked) {
+      ellipseMode(CENTER);
+      fill(255);
+      ellipse(bx,by,buttonSize+10,buttonSize+10);
+    }
+  } else {
+    //console.log("ur the 2nd best")
+    overButton = false;
+  }
     // if (mouseIsPressed){
     //   image(myVid,0,0);
     // } else {
@@ -57,7 +79,8 @@ function draw() {
     // }
 
   //function mouseClicked(){
-    image(myVid,0,0);
+    imageMode(CENTER);
+    image(myVid,width/2,height/2);
   //} 
   //function mainPage() {
   for (var i=0;i<objectResults.length;i++){
@@ -69,21 +92,51 @@ function draw() {
     
     text(obj.label, obj.x, obj.y-30);
 
-    //results
-    if (i === 1) {
-      text("this must be a: " +  obj.label, width/2, 100);
-      } else { 
-      text(`this must be either of these: ${objectResults[0].label}, ${objectResults[1].label}` , width/2, 100);
-      }
-    text("i am "+ round(obj.confidence * 100) + "% confident on this one!", width/2, height-20);
     
+    //if (i === 1) { //results
+    fill(255);
+    rect(200,10, width- 200, 90);
+    noFill();
+    var y = 50;
+      text("this must be a: " +  objectResults[0].label , width/2, y);
+      // } else { 
+      // text(`this must be either of these: ${objectResults[0].label}, ${objectResults[1].label}` , width/2, 100);
+      // }
+    text("i am "+ round(obj.confidence * 100) + "% confident on this one!", width/2, y+50);
+    text("Did I guess right?",width/2,height-50);
+    buttonYes.resize(buttonSize,buttonSize);
+
+    //BUTTONYES SETTINGS
+    bx = width/4;
+    by = height-50;
+    xOffset = 0.0;
+    yOffset = 0.0;
+    image(buttonYes, bx,by);
+    //image(buttonNo, width-bx,by);
+    fill(255);
+
+    
+    imageMode(CENTER);
+
+
  //}
 }
- 
-
-
   
-  
+}
+
+function mousePressed() {
+  if (overButton) {
+    locked = true;
+    console.log("ur soooo cooool");
+  } else {
+    locked = false;
+  }
+  console.log("ur soooo cooool2");
+  xOffset = mouseX - bx;
+  yOffset = mouseY - by;
+}
+function mouseReleased() {
+  locked = false;
 }
 
 function startPage(){
